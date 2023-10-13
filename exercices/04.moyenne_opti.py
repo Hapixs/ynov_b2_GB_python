4efadc064b6a0f4ff7dcd6d475c8fff1fd19de06
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-10-13 17:00:04 +0200

    update

--- exercices/04.moyenne_opti.py
+++ exercices/04.moyenne_opti.py
@@ -2,15 +2,17 @@ choix = "o"
 note = 0
 moyenne = 0
 liste_notes = []
+nb_zero = 0
 
 while (choix == "o"):
     note = float(input("Veuillez saisir une note : "))
     liste_notes.append(note)
     moyenne = moyenne + note
     
+    if (note == 0):
+        nb_zero += 1
+    
     choix = input("Voulez vous saisir une nouvelle note ? (o/n)")
     
-# AFFICHAGE
-    
 print(f"La moyenne de la classe est : {round(moyenne / len(liste_notes), 2)}")
-# La moyenne optimisée est de : XX (nb zéro : X)
+print(f"La moyenne de la classe est : {round(moyenne / (len(liste_notes) - nb_zero), 2)}")
