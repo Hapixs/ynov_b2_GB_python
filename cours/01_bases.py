ef0228fdd8ae282ef6783ddd63328dc79413ff85
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-10-13 14:27:12 +0200

    update

---
+++ cours/01_bases.py
@@ -0,0 +1,54 @@
+print("\n")
+
+x = 42 # int
+pi = 3.14 # float
+chaine = "Coucou" # str
+chaine = "Je m'appelle Geoffroy"
+phrase = """Je suis
+Une phrase
+Sur plusieurs lignes
+"""
+
+print(phrase)
+
+# opérateurs arithmétiques
+
+x = 42
+x = x + 1
+x += 1
+
+x = x - 1
+x -= 1
+
+x = x * 10
+x = x / 10
+
+print(20 // 6) # 3
+print(20 % 6) # 2
+print(20 ** 5)
+
+print(20 / 2 + 1)
+print(20 / (2 + 1))
+
+# Opérations sur les chaines de caractères
+
+chaine = "Python"
+chaine = chaine + " " + "3.12"
+print(chaine)
+print(chaine * 3)
+
+version = 3.12
+chaine = "Python" + str(version)
+print(chaine)
+print(version + 1)
+
+# type casting (conversation type)
+
+int
+float
+str
+
+print(type(3.14))
+
+version = str(version)
+print(type(version))
